7aa2051f417330860ec397eca2db3903a2ae3d08
= <lilyxmeng13@gmail.com>
2023-07-13 19:45:02 -0400

    Initialized Deck entity class

---
+++ src/main/java/entities/Deck.java
@@ -0,0 +1,15 @@
+package entities;
+
+import java.io.*;
+import java.util.*;
+
+public class Deck {
+
+    public static void main(String[] args) throws IOException {
+    }
+
+    public Card removeCard() {
+        Card card = new Card("", 0, null);
+        return card;
+    }
+}
