21bd248b68f9d2e25ebb074ede46ac3de65ccc5a
= <lilyxmeng13@gmail.com>
2023-07-13 19:44:48 -0400

    Initialized ComputerPlayer entity class

---
+++ src/main/java/entities/ComputerPlayer.java
@@ -0,0 +1,30 @@
+package entities;
+
+import java.io.*;
+import java.util.*;
+
+public class ComputerPlayer implements Player {
+
+    public static void main(String[] args) throws IOException {
+    }
+
+    @Override
+    public int getNumCard() {
+        return 0;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void putCard(Game game, Card card) {
+
+    }
+
+    @Override
+    public void setHand() {
+
+    }
+}
