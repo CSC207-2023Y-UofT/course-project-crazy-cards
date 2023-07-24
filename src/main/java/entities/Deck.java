1606a439af86eb0e2b08e42fbcc976f0395d4140
Solemann Mateen <solemann.mateen@mail.utoronto.ca>
2023-07-24 14:22:35 -0400

    Created Deck interface for OCP purposes, such that future users may implement their own non standard deck.

---
+++ src/main/java/entities/Deck.java
@@ -0,0 +1,10 @@
+package entities;
+
+// An interface with the method signatures of a Deck. An interface is used as future users of the program
+// may want to implement their own non-standard deck of Cards.
+public interface Deck {
+
+    void addCardToDeck(Card card);
+
+    void removeCardFromDeck(Card card);
+}
