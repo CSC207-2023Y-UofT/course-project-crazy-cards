8d1e0ed61c7aa8f8e00a5c054f12b2120b1237c5
nyan-ko <dumbstupidcatboy@gmail.com>
2023-07-26 22:02:06 -0400

    Design tentative Stats window layout

--- src/main/java/ui/windows/StatsRenderer.java
+++ src/main/java/ui/windows/StatsRenderer.java
@@ -1,15 +1,73 @@
 package ui.windows;
 
-import ui.StatsRendererData;
+import javax.swing.*;
 
-public class StatsRenderer {
+public class StatsRenderer extends JPanel {
+    private static final String EMPTY_LABEL = "";
+
     private StatsRendererData data;
+
+    private JLabel nameLabel;
+    private JLabel gamesPlayedLabel;
+    private JLabel gamesWonLabel;
+    private JLabel longestWinStreakLabel;
 
     public StatsRenderer(StatsRendererData data) {
         this.data = data;
+
+        initializeGUIComponents();
     }
 
-    public void display() {
-        
+    private void initializeGUIComponents() {
+        JPanel dataPanel = new JPanel();
+        dataPanel.setLayout(new BoxLayout(dataPanel, BoxLayout.Y_AXIS));
+        dataPanel.setAlignmentX(JPanel.LEFT_ALIGNMENT);
+
+        nameLabel = new JLabel("Enter a username.");
+        gamesPlayedLabel = new JLabel(EMPTY_LABEL);
+        gamesWonLabel = new JLabel(EMPTY_LABEL);
+        longestWinStreakLabel = new JLabel(EMPTY_LABEL);
+
+        dataPanel.add(nameLabel);
+        dataPanel.add(gamesPlayedLabel);
+        dataPanel.add(gamesWonLabel);
+        dataPanel.add(longestWinStreakLabel);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
+        JButton backButton = new JButton("Back");
+        JButton inputNameButton = new JButton("Search");
+        JTextField inputNameField = new JTextField(20);
+        buttonPanel.add(backButton);
+        buttonPanel.add(Box.createHorizontalGlue());
+        buttonPanel.add(inputNameButton);
+        buttonPanel.add(inputNameField);
+
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        add(dataPanel);
+        add(Box.createVerticalGlue());
+        add(buttonPanel);
     }
+
+    public void updateView() {
+        nameLabel.setText(data.getName());
+        gamesPlayedLabel.setText("Games Played: " + data.getGamesPlayed());
+        gamesWonLabel.setText("Games Won: " + data.getGamesWon());
+        longestWinStreakLabel.setText("Longest Win Streak: " + data.getLongestWinStreak());
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Stats Renderer");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 800);
+
+        StatsRendererData data = new StatsRendererData("Test", 10, 5, 3);
+        StatsRenderer renderer = new StatsRenderer(data);
+
+        renderer.setOpaque(true);
+        frame.setContentPane(renderer);
+        frame.setVisible(true);
+
+        renderer.updateView();
 }
