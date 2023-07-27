f42d07fc3e25870f45360560c94c3afe9fae82f0
nyan-ko <dumbstupidcatboy@gmail.com>
2023-07-26 22:04:40 -0400

    Add StatsDelegator class
    The GUI will follow an MVC style design pattern, with the data, display, and delegator serving the purpose of each object respectfully

---
+++ src/main/java/ui/windows/StatsDelegator.java
@@ -0,0 +1,16 @@
+package ui.windows;
+
+public class StatsDelegator {
+    private StatsRenderer renderer;
+    private StatsRendererData data;
+
+    public StatsDelegator() {
+
+    } 
+
+    public void requestUser(String username) {
+        // send usecase
+        // data.update
+        // renderer
+    }
+}
