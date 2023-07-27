753e40b7c357ccec2ec3d3cb333d2586147655e8
Nancy Hu <hunancymail@gmail.com>
2023-07-27 19:42:30 -0400

    Delete unused import

--- src/test/java/entities/HumanPlayerTest.java
+++ src/test/java/entities/HumanPlayerTest.java
@@ -2,7 +2,6 @@ package entities;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
