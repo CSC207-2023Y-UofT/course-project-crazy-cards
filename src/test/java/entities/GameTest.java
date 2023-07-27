24c58cfacb1579b3d5c1d8a086b8afe3d3448fb7
Nancy Hu <hunancymail@gmail.com>
2023-07-27 19:38:10 -0400

    Added access modifiers

--- src/test/java/entities/GameTest.java
+++ src/test/java/entities/GameTest.java
@@ -10,11 +10,11 @@ import java.util.ArrayList;
 
 class GameTest {
 
-    Game game;
-    Player p1;
-    Player p2;
-    ArrayList<Player> players;
-    Deck deck;
+    private Game game;
+    private Player p1;
+    private Player p2;
+    private ArrayList<Player> players;
+    private Deck deck;
 
     /**
      * Initialize a Game with a Deck and Players for testing of Game.java methods.
@@ -64,7 +64,7 @@ class GameTest {
      * Test that after a Player has played a Card, the current Card of the Game has been changed to the played Card.
      */
     @Test
-    void testPutCardDownAndGetCurrentCard() {
+    public void testPutCardDownAndGetCurrentCard() {
         // Get some Card from p1's Hand and put it down directly (we typically call putCardDown from p1.playCard,
         // but we have skipped that to test putCardDown directly).
         Card toPutDown = p1.getCards().get(0);
@@ -78,7 +78,7 @@ class GameTest {
      * Test that getPlayers() returns a list of all the Players in this Game.
      */
     @Test
-    void testGetPlayers() {
+    public void testGetPlayers() {
         ArrayList<Player> toTest = game.getPlayers();
         assertEquals(players, toTest);
     }
@@ -88,7 +88,7 @@ class GameTest {
      * Test that hasWinner(), when the Game doesn't have a winner, returns False.
      */
     @Test
-    void testHasWinnerNotYet() {
+    public void testHasWinnerNotYet() {
         boolean winnerYet = game.hasWinner();
         assertFalse(winnerYet);
     }
@@ -97,7 +97,7 @@ class GameTest {
      * Test that when a winner has been set, that getWinner() returns that Player.
      */
     @Test
-    void testGetAndSetWinner() {
+    public void testGetAndSetWinner() {
         game.setWinner(p1);
         assertEquals(p1, game.getWinner());
     }
@@ -106,7 +106,7 @@ class GameTest {
      * Test isValidCard() returns True when given a Card of same suit.
      */
     @Test
-    void testIsValidCardSameSuit() {
+    public void testIsValidCardSameSuit() {
         game.putCardDown(new Card("Spades", "9"));
         Card toTest = new Card("Spades", "7");
         boolean validity = game.isValidCard(toTest);
@@ -117,7 +117,7 @@ class GameTest {
      * Test isValidCard() returns True when given a Card of same value.
      */
     @Test
-    void testIsValidCardSameValue() {
+    public void testIsValidCardSameValue() {
         game.putCardDown(new Card("Hearts", "9"));
         Card toTest = new Card("Spades", "9");
         boolean validity = game.isValidCard(toTest);
@@ -128,7 +128,7 @@ class GameTest {
      * Test isValidCard() returns false when given an invalid Card.
      */
     @Test
-    void testIsValidCardInvalid() {
+    public void testIsValidCardInvalid() {
         game.putCardDown(new Card("Hearts", "9"));
         Card toTest = new Card("Spades", "King");
         boolean validity = game.isValidCard(toTest);
@@ -139,7 +139,7 @@ class GameTest {
      * Test that with a currentCard of different suit and number, any card with number 8 is valid.
      */
     @Test
-    void testIsValidCardCrazy8() {
+    public void testIsValidCardCrazy8() {
         game.putCardDown(new Card("Hearts", "9"));
         Card toTest = new Card("Spades", "8");
         boolean validity = game.isValidCard(toTest);
@@ -150,7 +150,7 @@ class GameTest {
      * Test that getGameDeck returns the Deck used in this Game.
      */
     @Test
-    void testGetGameDeck() {
+    public void testGetGameDeck() {
         Deck toTest = game.getGameDeck();
         assertEquals(deck, toTest);
     }
