f730c95c6f04df481aab59542fd57396fc0d43a3
Nancy Hu <hunancymail@gmail.com>
2023-07-31 15:36:30 -0400

    Added documentation

--- src/main/java/entities/HumanPlayerFactory.java
+++ src/main/java/entities/HumanPlayerFactory.java
@@ -1,9 +1,16 @@
 package entities;
 
 public class HumanPlayerFactory implements PlayerFactory {
+    /**
+     * Create a HumanPlayer with the given parameters.
+     * @param name The name of the Player that will be created.
+     * @param hand The hand dealt to the Player that will be created.
+     * @return Return a new HumanPlayer with the given name and hand, and default wins and losses.
+     */
     @Override
     public Player create(String name, Hand hand) {
         return new HumanPlayer(name, hand, 0, 0) {
         };
     }
+    // TODO: implement persistence for wins and losses of players who have played before
 }
