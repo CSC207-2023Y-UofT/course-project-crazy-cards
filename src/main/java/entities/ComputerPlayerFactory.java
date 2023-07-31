6530f13a15fc8cfc967051f524d9666068e983b6
Nancy Hu <hunancymail@gmail.com>
2023-07-31 15:36:35 -0400

    Added documentation

--- src/main/java/entities/ComputerPlayerFactory.java
+++ src/main/java/entities/ComputerPlayerFactory.java
@@ -1,6 +1,12 @@
 package entities;
 
 public class ComputerPlayerFactory implements PlayerFactory {
+    /**
+     * Create a ComputerPlayer with the given parameters.
+     * @param name The name of the Player that will be created.
+     * @param hand The hand dealt to the Player that will be created.
+     * @return Return a new ComputerPlayer with the given name and hand.
+     */
     @Override
     public Player create(String name, Hand hand) {
         return new ComputerPlayer(name, hand);
