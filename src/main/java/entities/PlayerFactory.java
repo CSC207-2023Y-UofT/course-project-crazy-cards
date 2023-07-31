9992fcec69b6f11b43b4617fff0c545ad7ecdb08
Nancy Hu <hunancymail@gmail.com>
2023-07-31 15:36:27 -0400

    Added documentation

--- src/main/java/entities/PlayerFactory.java
+++ src/main/java/entities/PlayerFactory.java
@@ -1,5 +1,10 @@
 package entities;
 
 public interface PlayerFactory {
+    /**
+     * Create a Player upon calling this method.
+     * @param name The name of the Player that will be created.
+     * @param hand The hand dealt to the Player that will be created.
+     */
     Player create(String name, Hand hand);
 }
