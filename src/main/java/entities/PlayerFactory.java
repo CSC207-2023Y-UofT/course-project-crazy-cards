89c2540e9d7b4144ea06d16531ff28528a9402fc
Nancy Hu <hunancymail@gmail.com>
2023-08-02 12:06:24 -0400

    Change comments to Javadocs

--- src/main/java/entities/PlayerFactory.java
+++ src/main/java/entities/PlayerFactory.java
@@ -1,7 +1,9 @@
 package entities;
 
-// The interface used to implement the different types of player factories that will be used
-// to create the players whenever a new game of Crazy Cards is started.
+/**
+ * The interface used to implement the different types of player factories that will be used
+ * to create the players whenever a new game of Crazy Cards is started.
+ */
 public interface PlayerFactory {
     /**
      * Create a Player upon calling this method.
