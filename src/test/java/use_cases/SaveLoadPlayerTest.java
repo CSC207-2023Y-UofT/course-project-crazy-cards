dc1b086c09c7b0c754967acc65c0240ec5efe15e
= <lilyxmeng13@gmail.com>
2023-08-04 21:02:14 -0400

    Edited SaveLoadPlayerTest to use the new class CSVDatabase.

--- src/test/java/use_cases/SaveLoadPlayerTest.java
+++ src/test/java/use_cases/SaveLoadPlayerTest.java
@@ -18,6 +18,8 @@ public class SaveLoadPlayerTest {
     private HumanPlayer p1;
     private HumanPlayer p2;
     private HumanPlayer p3;
+    
+    CSVDatabase database;
 
     /**
      * Construct 3 HumanPlayer objects to be used in the following tests.
@@ -32,6 +34,8 @@ public class SaveLoadPlayerTest {
         p1 = new HumanPlayer("Lily1", hand, 0 , 0);
         p2 = new HumanPlayer("Lily2", hand, 1, 2);
         p3 = new HumanPlayer("Lily1", hand, 3, 0);
+
+        database = new CSVDatabase();
     }
 
     /**
@@ -40,7 +44,7 @@ public class SaveLoadPlayerTest {
      */
     @AfterEach
     public void tearDown() throws IOException {
-        FileWriter file = new FileWriter("src/main/java/use_cases/players.csv");
+        FileWriter file = new FileWriter("src/main/java/database/players.csv");
         file.write("");
         file.close();
     }
@@ -51,8 +55,8 @@ public class SaveLoadPlayerTest {
      */
     @Test
     public void saveLoadOneValidPlayer() throws IOException {
-        SavePlayer.savePlayerToFile(p1);
-        String[] p4 = LoadPlayer.loadPlayerFromFile("Lily1");
+        database.savePlayer(p1);
+        String[] p4 = database.loadPlayer("Lily1");
 
         assert p4 != null;
         assertEquals(p1.getName(), p4[0]);
@@ -66,10 +70,10 @@ public class SaveLoadPlayerTest {
      */
     @Test
     public void saveLoadMultipleValidPlayer() throws IOException {
-        SavePlayer.savePlayerToFile(p1);
-        SavePlayer.savePlayerToFile(p2);
-        String[] p4 = LoadPlayer.loadPlayerFromFile("Lily1");
-        String[] p5 = LoadPlayer.loadPlayerFromFile("Lily2");
+        database.savePlayer(p1);
+        database.savePlayer(p2);
+        String[] p4 = database.loadPlayer("Lily1");
+        String[] p5 = database.loadPlayer("Lily2");
 
         assert p4 != null;
         assertEquals(p1.getName(), p4[0]);
@@ -88,7 +92,7 @@ public class SaveLoadPlayerTest {
      */
     @Test
     public void loadOneInvalidPlayer() throws IOException {
-        String[] p4 = LoadPlayer.loadPlayerFromFile("Lily3");
+        String[] p4 = database.loadPlayer("Lily3");
 
         assertNull(p4);
     }
@@ -99,8 +103,8 @@ public class SaveLoadPlayerTest {
      */
     @Test
     public void loadMultipleInvalidPlayer() throws IOException {
-        String[] p4 = LoadPlayer.loadPlayerFromFile("Lily3");
-        String[] p5 = LoadPlayer.loadPlayerFromFile("Lily4");
+        String[] p4 = database.loadPlayer("Lily3");
+        String[] p5 = database.loadPlayer("Lily4");
 
         assertNull(p4);
         assertNull(p5);
@@ -112,9 +116,9 @@ public class SaveLoadPlayerTest {
      */
     @Test
     public void saveLoadValidInvalidPlayer() throws IOException {
-        SavePlayer.savePlayerToFile(p1);
-        String[] p4 = LoadPlayer.loadPlayerFromFile("Lily1");
-        String[] p5 = LoadPlayer.loadPlayerFromFile("Lily3");
+        database.savePlayer(p1);
+        String[] p4 = database.loadPlayer("Lily1");
+        String[] p5 = database.loadPlayer("Lily3");
 
         assert p4 != null;
         assertEquals(p1.getName(), p4[0]);
@@ -130,8 +134,8 @@ public class SaveLoadPlayerTest {
      */
     @Test
     public void saveExistingPlayer() throws IOException {
-        boolean saveP1 = SavePlayer.savePlayerToFile(p1);
-        boolean saveP3 = SavePlayer.savePlayerToFile(p3);
+        boolean saveP1 = database.savePlayer(p1);
+        boolean saveP3 = database.savePlayer(p3);
 
         assertTrue(saveP1);
         assertFalse(saveP3);
