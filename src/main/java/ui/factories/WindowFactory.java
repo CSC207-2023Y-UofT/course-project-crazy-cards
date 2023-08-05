84a360fbe8a35c14ecca5c4aed7c10cbd279a63f
Nancy Hu <hunancymail@gmail.com>
2023-08-05 18:29:29 -0400

    Add Javadocs

--- src/main/java/ui/factories/WindowFactory.java
+++ src/main/java/ui/factories/WindowFactory.java
@@ -3,5 +3,9 @@ package ui.factories;
 import ui.windows.Window;
 
 public interface WindowFactory {
+    /**
+     * Method to be used to create a Window class upon being called.
+     * @return The window that will be created.
+     */
     Window createWindow();
 }
