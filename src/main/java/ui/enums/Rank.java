57495061975766f2c2e799a2d266b983fe3948b7
Nancy Hu <hunancymail@gmail.com>
2023-08-05 18:01:12 -0400

    Add Javadocs

--- src/main/java/ui/enums/Rank.java
+++ src/main/java/ui/enums/Rank.java
@@ -1,5 +1,8 @@
 package ui.enums;
 
+/**
+ * Ranks to be used for their corresponding card in gameplay.
+ */
 public enum Rank {
     ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING;
 }
