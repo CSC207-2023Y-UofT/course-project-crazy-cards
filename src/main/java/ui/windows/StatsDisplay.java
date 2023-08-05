ef3541a1a54e11e76144b453d298590f1b43dd46
Nancy Hu <hunancymail@gmail.com>
2023-08-05 11:03:31 -0400

    Edit font and text

--- src/main/java/ui/windows/StatsDisplay.java
+++ src/main/java/ui/windows/StatsDisplay.java
@@ -1,19 +1,20 @@
 package ui.windows;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 
 /**
  * A JPanel subclass representing the game's statistics window.
  */
 public class StatsDisplay extends JPanel {
     private static final String EMPTY_LABEL = "";
-
     private StatsDelegator delegator;
-
     private JLabel nameLabel;
     private JLabel gamesPlayedLabel;
     private JLabel gamesWonLabel;
     private JLabel longestWinStreakLabel;
+    private JLabel nextPlayer;
 
     /**
      * Construct a StatsDisplay displaying default data.
@@ -32,18 +33,27 @@ public class StatsDisplay extends JPanel {
         // The data panel displaying user statistics
         JPanel dataPanel = new JPanel();
         dataPanel.setLayout(new BoxLayout(dataPanel, BoxLayout.Y_AXIS));
-        dataPanel.setAlignmentX(JPanel.LEFT_ALIGNMENT);
+        dataPanel.setBorder(new EmptyBorder(new Insets(10, 10, 10, 10)));
+        Font font = new Font("Calibri", Font.BOLD, 20);
+        // dataPanel.setAlignmentX(JPanel.LEFT_ALIGNMENT);
 
         // Default labels
         nameLabel = new JLabel("Enter a username.");
+        nameLabel.setFont(font);
         gamesPlayedLabel = new JLabel(EMPTY_LABEL);
+        gamesPlayedLabel.setFont(font);
         gamesWonLabel = new JLabel(EMPTY_LABEL);
+        gamesWonLabel.setFont(font);
         longestWinStreakLabel = new JLabel(EMPTY_LABEL);
+        longestWinStreakLabel.setFont(font);
+        nextPlayer = new JLabel(EMPTY_LABEL);
+        nextPlayer.setFont(font);
 
         dataPanel.add(nameLabel);
         dataPanel.add(gamesPlayedLabel);
         dataPanel.add(gamesWonLabel);
         dataPanel.add(longestWinStreakLabel);
+        dataPanel.add(nextPlayer);
 
         // The button panel containing the back button and search bar
         JPanel buttonPanel = new JPanel();
@@ -53,36 +63,35 @@ public class StatsDisplay extends JPanel {
 
         // Link the search bar to the delegator
         inputNameField.addActionListener(delegator);
-        
+
         // Formatting/spacing TBD
         buttonPanel.add(backButton);
         buttonPanel.add(Box.createHorizontalGlue());
         buttonPanel.add(inputNameField);
 
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-        add(dataPanel);
-        add(Box.createVerticalGlue());
-        add(buttonPanel);
+        setLayout(new BorderLayout());
+        add(dataPanel, BorderLayout.CENTER);
+        add(buttonPanel, BorderLayout.PAGE_END);
     }
 
     /**
      * Update this instance's labels to reflect updated values in data.
      */
     public void updateView(StatsDisplayData data) {
-        nameLabel.setText(data.getName());
+        nameLabel.setText("User: " + data.getName());
         gamesPlayedLabel.setText("Games Played: " + data.getGamesPlayed());
         gamesWonLabel.setText("Games Won: " + data.getGamesWon());
         longestWinStreakLabel.setText("Longest Win Streak: " + data.getLongestWinStreak());
+        nextPlayer.setText("<html><br>Enter a new username to see statistics.</html>");
     }
 
     /**
      * Tests creation of a Stats window.
      */
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Stats Display");
+        JFrame frame = new JFrame("Statistics Display");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 800);
+        frame.setSize(400, 400);
 
         StatsController controller = new StatsController();
         StatsDelegator delegator = new StatsDelegator(controller);
