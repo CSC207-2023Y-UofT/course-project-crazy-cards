3a2297009cc5656cac36dad0a45ba48637f3745d
Nancy Hu <hunancymail@gmail.com>
2023-08-05 19:32:55 -0400

    Add Javadocs

--- src/main/java/ui/windows/Window.java
+++ src/main/java/ui/windows/Window.java
@@ -6,7 +6,15 @@ import javax.swing.JPanel;
  * Interface for windows in a CardLayout.
  */
 public interface Window {
+    /**
+     * Getter method for the identifier of any given Window.
+     * @return The identifier of the Window, as a String.
+     */
     String getIdentifier();
 
+    /**
+     * Getter method for the panel associated with any given Window.
+     * @return The JPanel of the window.
+     */
     JPanel getPanel();
 }
