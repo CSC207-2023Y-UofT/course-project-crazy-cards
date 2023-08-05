7d2fc21bf83f93e8d7cfb53acb8b456704aea877
= <lilyxmeng13@gmail.com>
2023-08-04 22:11:41 -0400

    Changed file locations from use_cases to database packages.

--- src/main/java/database/CSVDatabase.java
+++ src/main/java/database/CSVDatabase.java
@@ -1,4 +1,4 @@
-package use_cases;
+package database;
 
 import entities.HumanPlayer;
 import entities.PlayerInformation;
--- src/test/java/database/SaveLoadPlayerTest.java
+++ src/test/java/database/SaveLoadPlayerTest.java
@@ -1,4 +1,4 @@
-package use_cases;
+package database;
 
 import entities.*;
 import org.junit.jupiter.api.AfterEach;
