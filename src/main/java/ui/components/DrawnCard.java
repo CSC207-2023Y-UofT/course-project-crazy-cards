103e4dc88a321439f58682e3def774aa3b6d89ec
Nancy Hu <hunancymail@gmail.com>
2023-08-09 23:09:07 -0400

    Update the class to display actual card faces

--- src/main/java/ui/components/DrawnCard.java
+++ src/main/java/ui/components/DrawnCard.java
@@ -1,27 +1,29 @@
 package ui.components;
 
-import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.*;
+import java.util.HashMap;
 
 import ui.enums.Rank;
 import ui.enums.Suit;
 
-import javax.swing.BorderFactory;
-import javax.swing.JComponent;
-import javax.swing.JPanel;
+import javax.swing.*;
+
+import static ui.enums.Rank.*;
 
 public class DrawnCard extends JPanel {
     private static final int HIGHLIGHT_OFFSET = 15;
-
     private Suit suit;
     private Rank rank;
     private int index;
-
     private boolean highlighted;
+    private HashMap<Rank, String> rankToString;
 
     public DrawnCard(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
         highlighted = false;
-
+        rankToString = new HashMap<>();
+        generateRankToString();
         initializeGUIComponents();
     }
 
@@ -37,7 +39,7 @@ public class DrawnCard extends JPanel {
         return rank;
     }
 
-    public void getRank(Rank rank) {
+    public void setRank(Rank rank) {
         this.rank = rank;
     }
 
@@ -49,9 +51,68 @@ public class DrawnCard extends JPanel {
         this.index = index;
     }
 
+    public void generateRankToString() {
+        rankToString.put(Rank.ACE, "A");
+        rankToString.put(Rank.TWO, "2");
+        rankToString.put(Rank.THREE, "3");
+        rankToString.put(Rank.FOUR, "4");
+        rankToString.put(Rank.FIVE, "5");
+        rankToString.put(Rank.SIX, "6");
+        rankToString.put(Rank.SEVEN, "7");
+        rankToString.put(Rank.EIGHT, "8");
+        rankToString.put(Rank.NINE, "9");
+        rankToString.put(Rank.TEN, "10");
+        rankToString.put(Rank.JACK, "J");
+        rankToString.put(Rank.QUEEN, "Q");
+        rankToString.put(Rank.KING, "K");
+    }
+
     private void initializeGUIComponents() {
-        setPreferredSize(new Dimension(30, 50));
+        Dimension dimension = new Dimension(50, 65);
+        setPreferredSize(dimension);
         setBorder(BorderFactory.createLineBorder(Color.BLACK, 2, true));
+        // Define colours
+        Color red = new Color(180, 20, 20);
+        Color black = Color.BLACK;
+
+        // Define Integers (for layering's sake in the JLayeredPane)
+        Integer back = 1;
+        Integer front = 2;
+
+        // Define font to be used
+        Font font = new Font("monospaced", Font.BOLD, 37);
+
+        // Creating objects
+        JLayeredPane pane = new JLayeredPane();
+        pane.setPreferredSize(dimension);
+        String valueString = rankToString.get(this.rank);
+        JLabel value = new JLabel(valueString);
+        value.setFont(font);
+        value.setBounds(22, 8, 50, 65);
+        JLabel suits = new JLabel();
+        suits.setFont(font);
+        suits.setBounds(5, -18, 50, 65);
+        switch (this.suit) {
+            case CLUB: suits.setText("<html>&#9827;</html>");
+            suits.setForeground(black);
+            value.setForeground(red);
+            break;
+            case SPADE: suits.setText("<html>&#9824;</html>");
+            suits.setForeground(black);
+            value.setForeground(red);
+            break;
+            case HEART: suits.setText("<html>&#9829;</html>");
+            suits.setForeground(red);
+            value.setForeground(black);
+            break;
+            case DIAMOND: suits.setText("<html>&#9830;</html>");
+            suits.setForeground(red);
+            value.setForeground(black);
+            break;
+        }
+        pane.add(suits, back);
+        pane.add(value, front);
+        add(pane);
     }
 
     public void highlight() {
