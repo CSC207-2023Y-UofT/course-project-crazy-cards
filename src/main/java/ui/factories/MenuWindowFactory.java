c19b2b66f3c192d09652396b28353664ce89dc57
nyan-ko <dumbstupidcatboy@gmail.com>
2023-08-10 11:53:00 -0400

    Add menu window factory

--- src/main/java/ui/factories/MenuWindowFactory.java
+++ src/main/java/ui/factories/MenuWindowFactory.java
@@ -2,18 +2,15 @@ package ui.factories;
 
 import ui.windows.MenuDisplay;
 import ui.windows.MenuWindow;
-import ui.windows.PaneDelegator;
 import ui.windows.Window;
 
 public class MenuWindowFactory implements WindowFactory {
-    private PaneDelegator delegator;
 
     @Override
     public Window createWindow() {
-        MenuDisplay display = new MenuDisplay(delegator);
+        MenuDisplay display = new MenuDisplay();
         MenuWindow window = new MenuWindow(display);
 
         return window;
     }
-    
 }
