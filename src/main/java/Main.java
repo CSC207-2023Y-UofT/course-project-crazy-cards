b31a6599f748c54961ca3be13f0e6c3faf88ac29
nyan-ko <dumbstupidcatboy@gmail.com>
2023-08-12 02:15:35 -0400

    Fix Main
    Gets Main working and outlines rough initialization procedure for each class and window in the program.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,78 +1,83 @@
-import controllers.GameBridge;
+
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+
 import controllers.GameCreationController;
+import controllers.PlayerGameController;
 import database.CSVDatabase;
+import entities.*;
+import enums.Rank;
+import enums.Suit;
 import enums.WindowName;
-import ui.factories.*;
-import ui.windows.Window;
-import ui.windows.creation.CreationDisplay;
-import ui.windows.game.GameController;
-import ui.windows.game.GameDisplay;
+import ui.components.NavigationButton;
+import ui.factories.CreatorWindowFactory;
+import ui.factories.GameWindowFactory;
+import ui.factories.MenuWindowFactory;
+import ui.factories.RuleWindowFactory;
+import ui.factories.StatsWindowFactory;
 import ui.windows.layout_managers.CardLayoutManager;
-import ui.windows.layout_managers.PaneDelegator;
+import ui.windows.layout_managers.ICardLayoutManager;
+import ui.windows.menu.MenuWindow;
 import ui.windows.stats.StatsController;
 import ui.windows.stats.StatsDisplay;
+import ui.windows.layout_managers.PaneDelegator;
+import ui.windows.Window;
+import ui.windows.creation.CreationDisplay;
 import use_cases.DataAccess;
+import use_cases.GameCreationInputBoundary;
 import use_cases.GameCreationInteractor;
+import use_cases.GameState;
+import use_cases.PlayerGameInputBoundary;
 import use_cases.PlayerGameInteractor;
-
-import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
 
-        CardLayoutManager layoutManager = new CardLayoutManager(frame);
+        GameManager manager = new GameManager();
+        GameState state = new GameState(manager);
+        manager.addObserver(state);
+
+        DataAccess dataAccess = new CSVDatabase();
+
+        IObserverNotifier notifier = new ObserverNotifier(manager);
+
+        PlayerGameInputBoundary gameBoundary = new PlayerGameInteractor(manager, notifier, state);
+        GameCreationInputBoundary creationBoundary = new GameCreationInteractor(dataAccess, manager);
+
+        PlayerGameController gameController = new PlayerGameController(gameBoundary);
+        GameCreationController creationController = new GameCreationController(creationBoundary);
+
+        ICardLayoutManager layoutManager = new CardLayoutManager(frame);
 
         MenuWindowFactory menuFactory = new MenuWindowFactory();
         RuleWindowFactory ruleFactory = new RuleWindowFactory();
-        StatsWindowFactory statsFactory = new StatsWindowFactory();
-        GameWindowFactory factory = new GameWindowFactory();
-        CreationWindowFactory creationWindowFactory = new CreationWindowFactory();
+        StatsWindowFactory statsFactory = new StatsWindowFactory(new CSVDatabase());
+        GameWindowFactory gameFactory = new GameWindowFactory(gameController);
+        CreatorWindowFactory creatorFactory = new CreatorWindowFactory(creationController);
 
         Window menuWindow = menuFactory.createWindow();
         Window ruleWindow = ruleFactory.createWindow();
         Window statsWindow = statsFactory.createWindow();
-        Window creationWindow = creationWindowFactory.createWindow();
-        DataAccess database = new CSVDatabase();
-        CreationDisplay creationDisplay = (CreationDisplay) creationWindow.getPanel();
-        GameCreationController creationController =  creationDisplay.getController();
-        GameCreationInteractor creationInteractor = creationController.getInteractor();
-        creationInteractor.setDataAccess(database);
-        StatsDisplay statsDisplay = (StatsDisplay) statsWindow.getPanel();
-        StatsController controller = statsDisplay.getDelegator().getController();
-        controller.setDatabase(database);
-        Window gameWindow = factory.createWindow();
+        Window gameWindow = gameFactory.createWindow();
+        Window creatorWindow = creatorFactory.createWindow();
 
         layoutManager.addPane(menuWindow);
         layoutManager.addPane(ruleWindow);
         layoutManager.addPane(statsWindow);
         layoutManager.addPane(gameWindow);
-        layoutManager.addPane(creationWindow);
+        layoutManager.addPane(creatorWindow);
 
         PaneDelegator paneDelegator = new PaneDelegator(layoutManager);
         menuWindow.setNavigator(paneDelegator);
         statsWindow.setNavigator(paneDelegator);
         ruleWindow.setNavigator(paneDelegator);
-
-        GameDisplay gameDisplay = (GameDisplay) gameWindow.getPanel();
-        GameController gameController = gameDisplay.getController();
-        creationDisplay.setGameController(gameController);
-        creationDisplay.setLayoutManager(layoutManager);
-
+        creatorWindow.setNavigator(paneDelegator);
 
         frame.setVisible(true);
-
-        PlayerGameInteractor playerGameInteractor = null;
-        while (playerGameInteractor == null) {
-            playerGameInteractor = creationController.getPlayerGameInteractor();
-        }
-        gameController.setBridge((GameBridge) playerGameInteractor);
-
-
-        layoutManager.setPane(WindowName.GAME);
     }
 }
