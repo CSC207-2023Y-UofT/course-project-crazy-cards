1f36a6e03d5671c74b082a655666d6b8fdbaf8b4
Nancy Hu <hunancymail@gmail.com>
2023-08-12 17:08:14 -0400

    Add documentation and new How to Play button

--- src/main/java/ui/windows/menu/MenuDisplay.java
+++ src/main/java/ui/windows/menu/MenuDisplay.java
@@ -12,17 +12,30 @@ public class MenuDisplay extends JPanel {
     private NavigationButton playButton;
     private NavigationButton statsButton;
     private NavigationButton rulesButton;
+    private NavigationButton howtoButton;
 
+    /**
+     * Constructor to create the necessary GUI components to display the main menu/title screen.
+     */
     public MenuDisplay() {
         initializeGUIComponents();
     }
 
+    /**
+     * Add ActionListener to each button on the main menu.
+     * @param navigator The PaneDelegator to be added to each button.
+     */
     public void setNavigator(PaneDelegator navigator) {
         playButton.addActionListener(navigator);
         statsButton.addActionListener(navigator);
         rulesButton.addActionListener(navigator);
+        howtoButton.addActionListener(navigator);
     }
 
+    /**
+     * Initialize the GUI components required to display the GUI components, including the title, logo, and buttons to
+     * navigate between screens/windows.
+     */
     private void initializeGUIComponents() {
         JPanel main = new JPanel();
         JPanel buttonPanel = new JPanel();
@@ -47,10 +60,12 @@ public class MenuDisplay extends JPanel {
         playButton = new NavigationButton(WindowName.CREATOR, "Play");
         statsButton = new NavigationButton(WindowName.STATS, "Stats");
         rulesButton = new NavigationButton(WindowName.RULES, "Rules");
+        howtoButton = new NavigationButton(WindowName.HOWTO, "How to Play");
 
         buttonPanel.add(playButton);
         buttonPanel.add(statsButton);
         buttonPanel.add(rulesButton);
+        buttonPanel.add(howtoButton);
 
         setLayout(new BorderLayout());
         add(main, BorderLayout.NORTH);
