f31169f653eb8ec61e0cf1a13e91053992df4f64
Nancy Hu <hunancymail@gmail.com>
2023-08-12 18:37:05 -0400

    Add javadocs

--- src/main/java/enums/WindowName.java
+++ src/main/java/enums/WindowName.java
@@ -1,5 +1,8 @@
 package enums;
 
+/**
+ * Enums used to identify each type of window.
+ */
 public enum WindowName {
     MENU("Menu"),
     GAME("Game"),
@@ -9,10 +12,18 @@ public enum WindowName {
 
     private String name;
 
+    /**
+     * Constructor to give a given window the correct enum associated with it.
+     * @param name The enum associated with a given window.
+     */
     WindowName(String name) {
         this.name = name;
     }
 
+    /**
+     * Getter method that returns the enum associated with a given window.
+     * @return The enum associated with any given window.
+     */
     public String getName() {
         return name;
     }
