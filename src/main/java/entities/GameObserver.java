5374e4b822eece9a483b112bbbcb2c4b4a13b154
nyan-ko <dumbstupidcatboy@gmail.com>
2023-08-11 22:18:50 -0400

    Move observable interface attributes
    These attributes are already declared in the GameAccess interface and are thus unnecessary.

--- src/main/java/entities/GameObserver.java
+++ src/main/java/entities/GameObserver.java
@@ -6,6 +6,6 @@ public interface GameObserver {
      * Update this GameObserver observing the given ObservableGame.
      * @param game An ObservableGame that has notified this GameObserver of an update.
      */
-    void updateGameObserver(ObservableGame game);
+    void updateGameObserver(GameAccess game);
 
 }
