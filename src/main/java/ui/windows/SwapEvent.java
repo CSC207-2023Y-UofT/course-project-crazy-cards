e573ccd432428590dc8617ed8225616b26d64bd9
nyan-ko <dumbstupidcatboy@gmail.com>
2023-08-12 18:11:42 -0400

    Add SwapListener related documentation

--- src/main/java/ui/windows/SwapEvent.java
+++ src/main/java/ui/windows/SwapEvent.java
@@ -2,19 +2,35 @@ package ui.windows;
 
 import enums.WindowName;
 
+/**
+ * An event that stores swapping information.
+ */
 public class SwapEvent {
     private WindowName previous;
     private WindowName next;
 
+    /**
+     * Construct a new SwapEvent given the previous and next window names.
+     * @param previous The name of the last window.
+     * @param next The name of the window being swapped to.
+     */
     public SwapEvent(WindowName previous, WindowName next) {
         this.previous = previous;
         this.next = next;
     }
 
+    /**
+     * Getter method for the previous window name.
+     * @return The name of the previous window.
+     */
     public WindowName getPrevious() {
         return previous;
     }
 
+    /**
+     * Getter method for the next window name.
+     * @return The name of the next window.
+     */
     public WindowName getNext() {
         return next;
     }
--- src/main/java/ui/windows/game/SwitchUpdater.java
+++ src/main/java/ui/windows/game/SwitchUpdater.java
@@ -1,12 +1,22 @@
 package ui.windows.game;
 
+/**
+ * Updates the game window when switched to.
+ */
 public class SwitchUpdater {
     private GameController controller;
 
+    /**
+     * Construct a new SwitchUpdater given a GameController.
+     * @param controller The GameController belonging to this SwitchUpdater.
+     */
     public SwitchUpdater(GameController controller) {
         this.controller = controller;
     }
 
+    /**
+     * Updates the game window when switched to.
+     */
     public void update() {
         controller.requestStart();
     }
