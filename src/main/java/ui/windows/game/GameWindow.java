b3551dc1a174685b3ef1bcc144e8bd5e36e336d8
Nancy Hu <hunancymail@gmail.com>
2023-08-12 21:07:38 -0400

    Add javadocs

--- src/main/java/ui/windows/game/GameWindow.java
+++ src/main/java/ui/windows/game/GameWindow.java
@@ -6,21 +6,32 @@ import enums.WindowName;
 import ui.windows.SwapEvent;
 import ui.windows.SwapListener;
 import ui.windows.Window;
-import ui.windows.game.GameDisplay;
 import ui.windows.layout_managers.PaneDelegator;
 
 public class GameWindow implements Window, SwapListener {
     private GameDisplay display;
 
+    /**
+     * Constructor for the window that displays the actual game.
+     * @param display The GameDisplay the window is connected to.
+     */
     public GameWindow(GameDisplay display) {
         this.display = display;
     }
 
+    /**
+     * Getter method for the identifier associated with the window.
+     * @return The identifier for the window, as an enum.
+     */
     @Override
     public WindowName getIdentifier() {
         return WindowName.GAME;
     }
 
+    /**
+     * Getter method that returns the display of the page.
+     * @return Returns the display of the Game being played on.
+     */
     @Override
     public JPanel getPanel() {
         return display;
@@ -29,9 +40,13 @@ public class GameWindow implements Window, SwapListener {
     @Override
     public void setNavigator(PaneDelegator navigator) {
         // add when game window has navigation, if ever
-        // if not needed, extract setNavigator into Navigable interface or smth
+        // if not needed, extract setNavigator into Navigable interface or something else
     }
 
+    /**
+     * Listens for SwapEvents, and notifies controller.
+     * @param event The event containing the window swap information.
+     */
     @Override
     public void OnPreSwap(SwapEvent event) {
         display.onSwitch();
