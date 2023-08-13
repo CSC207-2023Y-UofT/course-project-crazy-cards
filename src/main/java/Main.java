e274c4c8e6abf7ffd9bff0315f3b74bb963ab36c
= <lilyxmeng13@gmail.com>
2023-08-12 21:24:59 -0400

    Added Javadocs for Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,18 @@
-
-import java.awt.event.ActionEvent;
-import java.util.ArrayList;
-
 import javax.swing.JFrame;
 
 import controllers.GameCreationController;
 import controllers.PlayerGameController;
 import database.CSVDatabase;
 import entities.*;
-import enums.Rank;
-import enums.Suit;
 import enums.WindowName;
-import ui.components.NavigationButton;
 import ui.factories.*;
-import ui.windows.layout_managers.CardLayoutManager;
-import ui.windows.layout_managers.ICardLayoutManager;
-import ui.windows.menu.MenuWindow;
-import ui.windows.stats.StatsController;
-import ui.windows.stats.StatsDisplay;
-import ui.windows.layout_managers.PaneDelegator;
+import ui.windows.layout_managers.*;
 import ui.windows.Window;
-import ui.windows.creator.CreatorDisplay;
-import use_cases.DataAccess;
-import use_cases.GameCreationInputBoundary;
-import use_cases.GameCreationInteractor;
-import use_cases.GameState;
-import use_cases.PlayerGameInputBoundary;
-import use_cases.PlayerGameInteractor;
+import use_cases.*;
 
+/**
+ * Main class to run the game.
+ */
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
