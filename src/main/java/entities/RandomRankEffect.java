578190ce38250c7a83aae40dfc022cca0f747557
nyan-ko <dumbstupidcatboy@gmail.com>
2023-08-14 14:45:07 -0400

    Add documentation

--- src/main/java/entities/RandomRankEffect.java
+++ src/main/java/entities/RandomRankEffect.java
@@ -2,20 +2,35 @@ package entities;
 
 import enums.Rank;
 
+/**
+ * This class represents a special effect that changes the current rank to a random rank.
+ */
 public class RandomRankEffect implements SpecialEffect {
     private Rank[] ranks = {Rank.ACE, Rank.TWO, Rank.THREE, Rank.FOUR, Rank.FIVE, Rank.SIX, Rank.SEVEN, Rank.EIGHT, Rank.NINE, Rank.TEN, Rank.JACK, Rank.QUEEN, Rank.KING};
     private boolean alwaysPlayable;
 
+    /**
+     * Constructs a RandomRankEffect.
+     * @param alwaysPlayable Whether a card with this effect is always playable.
+     */
     public RandomRankEffect(boolean alwaysPlayable) {
         this.alwaysPlayable = alwaysPlayable;
     }
 
+    /**
+     * Changes the current rank to a random rank.
+     * @param game The game to apply the effect to.
+     */
     @Override
     public void action(GameAccess game) {
         Rank random = ranks[(int) (Math.random() * ranks.length)];
         game.setCurrentRank(random);
     }
 
+    /**
+     * Returns whether a card with this effect is always playable.
+     * @return Whether a card with this effect is always playable.
+     */
     @Override
     public boolean getAlwaysPlayable() {
         return this.alwaysPlayable;
--- src/main/java/entities/RandomSuitEffect.java
+++ src/main/java/entities/RandomSuitEffect.java
@@ -2,20 +2,35 @@ package entities;
 
 import enums.Suit;
 
+/**
+ * This class represents a special effect that changes the current suit to a random suit.
+ */
 public class RandomSuitEffect implements SpecialEffect {
     private Suit[] suits = {Suit.SPADE, Suit.HEART, Suit.CLUB, Suit.DIAMOND};
     private boolean alwaysPlayable;
 
+    /**
+     * Constructs a RandomSuitEffect.
+     * @param alwaysPlayable Whether a card with this effect is always playable.
+     */
     public RandomSuitEffect(boolean alwaysPlayable) {
         this.alwaysPlayable = alwaysPlayable;
     }
 
+    /**
+     * Changes the current suit to a random suit.
+     * @param game The game to apply the effect to.
+     */
     @Override
     public void action(GameAccess game) {
         Suit random = suits[(int) (Math.random() * suits.length)];
         game.setCurrentSuit(random);
     }
 
+    /**
+     * Returns whether a card with this effect is always playable.
+     * @return Whether a card with this effect is always playable.
+     */
     @Override
     public boolean getAlwaysPlayable() {
         return this.alwaysPlayable;
