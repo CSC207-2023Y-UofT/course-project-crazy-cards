4434a5a33a7c4736898e4abc60f0c489a0da079b
nyan-ko <dumbstupidcatboy@gmail.com>
2023-08-15 13:30:10 -0400

    Add javadocs

--- src/main/java/controllers/interfaces/GameUI.java
+++ src/main/java/controllers/interfaces/GameUI.java
@@ -1,7 +1,14 @@
-package ui.interfaces;
+package controllers.interfaces;
 
 import controllers.data_objects.GameDisplayData;
 
+/**
+ * Represents an active game renderer.
+ */
 public interface GameUI {
+    /** 
+     * Update the view with the given data.
+     * @param data The data to be displayed.
+     */
     void updateView(GameDisplayData data);
 }
